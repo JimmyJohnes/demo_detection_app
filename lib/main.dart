66f5fc85d2c0d6289f6a764712e7090c64676cd7
JimmyJohnes <amsoliman03@outlook.com>
2023-12-17 20:21:18 +0200

    added featuer: show list of detected classes TODO: add feature: run object detection from live cam feed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:pytorch_lite/pytorch_lite.dart';
 import 'package:image_picker_android/image_picker_android.dart';
+import 'package:demo_detection_app/counted_objects.dart';
 import 'dart:io';
 
 void main() {
@@ -15,10 +16,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
       home: const MyHomePage(title: 'Demo Detection App'),
     );
   }
@@ -35,48 +32,76 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-	Widget i  = Image.asset("assets/labels/Milk-Carton-Packaging-Mockup.jpg");
-	Image dk = Image.asset("assets/labels/Milk-Carton-Packaging-Mockup.jpg");
+	Widget image  = Image.asset("assets/labels/Milk-Carton-Packaging-Mockup.jpg",fit: BoxFit.fill);
+	List<String?> classNames = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        backgroundColor: const Color.fromARGB(255, 70, 0, 100),
+	shadowColor: Colors.grey,
+        title: Center(child: Text(widget.title, style: const TextStyle(
+		color: Colors.white,
+		fontSize: 24.0,
+		fontWeight: FontWeight.bold,
+	),)),
       ),
-      body: Container(
-      width: 250,
-      height: 250,
-        child: Stack(
-		children: [
-			i
-		],
-	),
+      body: Column(
+        children: [
+		  Center(
+			    child: Container(
+			    width: 300,
+			    height: 300,
+				padding: const EdgeInsets.all(20),
+				decoration: BoxDecoration(
+					borderRadius: const BorderRadius.all(Radius.circular(50)),
+					border: Border.all(color: Colors.white),
+				) ,
+			      child: image
+			),
+		),
+		ElevatedButton(onPressed: (){
+			Navigator.push(context, 
+			MaterialPageRoute(builder: (_) => CountObjects(objNames: classNames))
+			);
+		}, child: Text("View Counted Classes"))
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-	  ModelObjectDetection model = await PytorchLite.loadObjectDetectionModel(
-		"assets/model/best(1).torchscript",
-		10, 640, 640,
-		objectDetectionModelType: ObjectDetectionModelType.yolov8,
-		labelPath: "assets/labels/labels.txt"
-		);
-		PickedFile? imageFile = await ImagePickerAndroid().pickImage(source: ImageSource.gallery);
-	 var img = await File(imageFile!.path).readAsBytes();
-	 List<ResultObjectDetection> objDetect = await model.getImagePrediction(img,minimumScore: 0.1,iOUThreshold: 0.3);
-	 print("=======================\n");
-	 for (var i in objDetect){
-	 	print(i.className);
-	 }
-	 File foo = File(imageFile.path);
-	 i = model.renderBoxesOnImage(foo, objDetect);
-	 dk = Image.file(foo);
+      backgroundColor: const Color.fromARGB(255, 70, 0, 125),
+      floatingActionButton: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+		FloatingActionButton(
+		onPressed: () async {
+		},
+		tooltip: "Open From Camera",
+		child: const Icon(Icons.camera),
+	      ), // This trailing comma makes auto-formatting nicer for build methods.
+	      FloatingActionButton(
+		onPressed: () async {
+		  ModelObjectDetection model = await PytorchLite.loadObjectDetectionModel(
+			"assets/model/best(1).torchscript",
+			10, 640, 640,
+			objectDetectionModelType: ObjectDetectionModelType.yolov8,
+			labelPath: "assets/labels/labels.txt"
+			);
+			PickedFile? imageFile = await ImagePickerAndroid().pickImage(source: ImageSource.gallery);
+		 var img = await File(imageFile!.path).readAsBytes();
+	         List<ResultObjectDetection> objDetect = await model.getImagePrediction(img,minimumScore: 0.1,iOUThreshold: 0.3);
+		 classNames = [];
+		 for (var i in objDetect){
+		 	classNames.add(i.className);
+		 }
+		 File foo = File(imageFile.path);
 		setState(() {
-				});
-	},
-        tooltip: 'Open Camera',
-        child: const Icon(Icons.camera),
+			 image = model.renderBoxesOnImage(foo, objDetect);
+			});
+		},
+		tooltip: 'Open From Gallery',
+		child: const Icon(Icons.photo),
       ), // This trailing comma makes auto-formatting nicer for build methods.
+      ],
+      )
     );
   }
 }
